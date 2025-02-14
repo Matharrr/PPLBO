aba9ed97993d9f1d2e860e1ba468acf9e9833d9b
Matharrr <matharrizqi@gmail.com>
2025-02-14 14:32:39 +0700

    inventory part 3

---
+++ inventory-service/src/main/java/com/mathar/microservice/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.mathar.microservice.inventory.repository;
+
+import com.mathar.microservice.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
